0b653071c2b302b99d83dfeabafd947edc97b6c1
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-14 16:34:19 +0530

    Create guvi53.java

---
+++ guvi53.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	static int getSum(int n)
+    {    
+        int sum = 0;
+         
+        while (n != 0)
+        {
+            sum = sum + n % 10;
+            n = n/10;
+        }
+     
+    return sum;
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n = 123;
+ 
+        System.out.println(getSum(n));
+ }
+}
